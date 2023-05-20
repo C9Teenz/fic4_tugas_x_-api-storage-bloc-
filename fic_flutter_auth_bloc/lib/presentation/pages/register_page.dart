548acaa8b3113fa1ed9aa796c7c930affa1c88e8
Munir <munirkun123@gmail.com>
2023-05-20 17:30:52 +0700

    bloc

---
+++ fic_flutter_auth_bloc/lib/presentation/pages/register_page.dart
@@ -0,0 +1,161 @@
+import 'package:fic4_flutter_auth_bloc/bloc/register/register_bloc.dart';
+import 'package:fic4_flutter_auth_bloc/data/models/request/register_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController? nameController;
+  TextEditingController? emailController;
+  TextEditingController? passwordController;
+  bool isSecured = true;
+  @override
+  void initState() {
+    nameController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController!.dispose();
+    emailController!.dispose();
+    passwordController!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.text,
+              controller: nameController,
+              decoration: InputDecoration(
+                focusColor: Colors.brown[300],
+                prefixIcon: const Icon(
+                  Icons.person_outline,
+                  size: 28,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                labelText: 'Enter Your Name',
+                labelStyle: TextStyle(
+                    color: Colors.grey[400], fontWeight: FontWeight.w400),
+                prefixIconColor: Colors.brown[300],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              controller: emailController,
+              decoration: InputDecoration(
+                focusColor: Colors.brown[300],
+                prefixIcon: const Icon(
+                  Icons.email_outlined,
+                  size: 28,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                labelText: 'Enter Your Email',
+                labelStyle: TextStyle(
+                    color: Colors.grey[400], fontWeight: FontWeight.w400),
+                prefixIconColor: Colors.brown[300],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              controller: passwordController,
+              obscureText: isSecured,
+              decoration: InputDecoration(
+                suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isSecured = !isSecured;
+                      });
+                    },
+                    child: Icon(
+                        isSecured ? Icons.visibility_off : Icons.visibility)),
+                focusColor: Colors.brown[300],
+                prefixIcon: const Icon(
+                  Icons.key,
+                  size: 28,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                labelText: 'Enter Your Password',
+                labelStyle: TextStyle(
+                    color: Colors.grey[400], fontWeight: FontWeight.w400),
+                prefixIconColor: Colors.brown[300],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            BlocConsumer<RegisterBloc, RegisterState>(
+              listener: (context, state) {
+                if (state is RegisterLoaded) {
+                  nameController!.clear();
+                  emailController!.clear();
+                  passwordController!.clear();
+                  //navigasi
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text(
+                            'success register with id: ${state.model.id}')),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state is RegisterLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ElevatedButton(
+                  onPressed: () {
+                    final requestModel = RegisterModel(
+                      name: nameController!.text,
+                      email: emailController!.text,
+                      password: passwordController!.text,
+                    );
+
+                    context
+                        .read<RegisterBloc>()
+                        .add(SaveRegisterEvent(request: requestModel));
+                  },
+                  child: const Text('Register'),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
