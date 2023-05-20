e0ae25c0c19c6947a0d4563047a3d61bfca4d3ff
Munir <munirkun123@gmail.com>
2023-05-20 16:54:02 +0700

    shared pref

---
+++ fic_sharepreferences_example/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_binar_localstorage/counter_shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Jagoflutter Local Storage',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Jagoflutter Gmeet ke-4'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _getDataCounter() async {
+    _counter = await CounterSharedPreferences().getData();
+    setState(() {});
+  }
+
+  void _incrementCounter() async {
+    await CounterSharedPreferences().saveData(++_counter);
+    _getDataCounter();
+  }
+
+  void _decrementCounter() async {
+    await CounterSharedPreferences().saveData(--_counter);
+    _getDataCounter();
+  }
+
+  void _removeCounter() async {
+    await CounterSharedPreferences().removeDate();
+    _getDataCounter();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getDataCounter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Value',
+                style: TextStyle(
+                    color: Colors.green[600],
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600)),
+            Text(
+              '$_counter',
+              style: TextStyle(
+                  color: Colors.green[600],
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: _decrementCounter, child: const Text('-')),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                    onPressed: _incrementCounter, child: const Text('+'))
+              ],
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: () {
+                _removeCounter();
+              },
+              child: const Text('Clear'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
